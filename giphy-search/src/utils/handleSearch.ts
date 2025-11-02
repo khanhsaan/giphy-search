807b4850bed4a5a356e4e1ef35d6a13f511add7f
khanhsaan <anthonykhanhan@gmail.com>
2025-11-02 20:44:41 +1100

    refactor: modified handleSearch()

---
+++ giphy-search/src/utils/handleSearch.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+const handleSearch = async(
+    query: string,
+    setLoading:(loading: boolean) => void,
+    setError: (error: string) => void,
+    setGifs:(gifs: any[])=> void
+) => {
+    const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
+    // check if the query is not empty
+    if(!query.trim()){
+        return ;
+    }
+
+    setLoading(true);
+    setError('');
+
+    try{
+        const response = await axios.get(
+            `https://api.giphy.com/v1/gifs/search`,
+            {
+                params: {
+                    // eslint-disable-next-line camelcase
+                    api_key: API_KEY,
+                    q: query,
+                    limit: 20
+                }
+            }
+        );
+        if(response.data){
+            console.log(response.data)
+        } else {
+            console.warn('There is no response!')
+        }
+        setGifs(response.data.data);
+    } catch (err){
+        setError('Failed to fetch GIFs');
+        console.error('Error fetching GIFs: ', err);
+    } finally {
+        setLoading(false);
+    }
+}
+
+export default handleSearch;
