e5c02bc3e9a1f461ab289c47e5e142a8a84c8e05
khanhsaan <anthonykhanhan@gmail.com>
2025-11-02 19:57:45 +1100

    feat: create interface GifObject

--- giphy-search/src/components/GiphySearch.tsx
+++ giphy-search/src/components/GiphySearch.tsx
@@ -1,16 +1,34 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import handleSearch from "../../utils/handleSearch";
 
-
+// from: https://developers.giphy.com/docs/api/endpoint/#search
+interface GifObject {
+    id: string,
+    images: {
+        fixed_height: {
+            url: string;
+        }
+    };
+    url: string;
+    bitly_url: string;
+}
 const GiphySearch: React.FC = () => {
     const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
     const[query, setQuery] = useState<string>('');
+    const[loading, setLoading] = useState<Boolean>(false);
+    const[error, setError] = useState<string>('');
+    const[gifs, setGifs] = useState<any[]>([]);
+
     return (
         <div>
             <h1>Giphy Search</h1>
 
-            <form>
+            <form
+                onSubmit={() => {
+                    handleSearch(query)
+                }}>
                 <input
                     type="text"
                     value={query}
